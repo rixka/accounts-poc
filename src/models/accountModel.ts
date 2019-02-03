09d6f6d15bbcaa2456ee7ba40f29d2c973c12d64
Kayleigh Rix <rixka@outlook.com>
2019-02-03 20:14:34 +0100

    [add] email validator
    Prevents setting put an account without a valid email.

--- src/models/accountModel.ts
+++ src/models/accountModel.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from 'express'
+import { isEmail } from 'validator'
 import * as mongoose from 'mongoose'
 
 // TODO: use env vars
@@ -10,7 +10,15 @@ mongoose.connect(uri, {
 })
 
 export const AccountSchema = new mongoose.Schema({
-  email: { type: String, unique: true, required: true },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    validate: {
+      validator: email => validator.isEmail(email),
+      message: '{VALUE} is not a valid email'
+    }
+  },
 })
 
 const Account = mongoose.model('Account', AccountSchema)
